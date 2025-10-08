dd6da373a8297c6e2c31a11f8865c2c333c11b8b
jumashafara <jumashafara0@gmail.com>
2025-10-08 11:01:19 +0300

    fix: removed load and save model

--- dataidea/sklearn/__init__.py
+++ dataidea/sklearn/__init__.py
@@ -1,6 +1,6 @@
 """
 Model management functionality
 """
-from dataidea.sklearn.models import load_model, save_model, regression_report, classification_report
+from dataidea.sklearn.models import regression_report, classification_report
 
-__all__ = ['load_model', 'save_model', 'regression_report', 'classification_report'] 
+__all__ = ['regression_report', 'classification_report'] 
